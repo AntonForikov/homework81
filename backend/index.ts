b2a369f04ab47b13215b4de11f9ef0cca9728600
Anton Forikov <tohabass@gmail.com>
2024-04-10 20:33:34 +0600

    backend: refactor index on mongoose

--- backend/index.ts
+++ backend/index.ts
@@ -1,21 +1,26 @@
 import express from 'express';
 import cors from 'cors';
-
+import shorterRouter from './routes/shorter';
+import mongoose from 'mongoose';
+import config from './config';
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 app.use(cors({origin: ['http://localhost:5173']}));
-// app.use('/comments', commentRouter);
+app.use('/links', shorterRouter);
 
-// const run = async () => {
-//   await fileDB.initNews();
-//   await fileDB.initComments();
-//
-//   app.listen(port, () => {
-//     console.log(`Server running on ${port} port.`);
-//   });
-// };
-//
-// void run();
+const run = async () => {
+  await mongoose.connect(config.mongoose.db);
+
+  app.listen(port, () => {
+    console.log(`Server running on ${port} port.`);
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+};
+
+void run();
