7a861941a987a6e9704047d96a5c294224d99bf2
Anton Forikov <tohabass@gmail.com>
2024-04-11 14:58:36 +0600

    frontend: refactor on a tag

--- frontend/src/components/Home/Answer.tsx
+++ frontend/src/components/Home/Answer.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import {Typography} from '@mui/material';
+import axiosApi from '../../axiosAPI';
 
 interface Props {
   shortUrl: string,
   originalUrl: string
 }
 const Answer: React.FC<Props> = ({shortUrl, originalUrl}) => {
+  const redirectByShortId = async () => {
+    await axiosApi.get(`/links/${shortUrl}`);
+  };
+
   return (
     <>
       <Typography variant='h4' marginY={3}>Your link now looks like this:</Typography>
-      <a href={originalUrl} target='_blank'>http://localhost:8000/{shortUrl}</a>
+      <a href={originalUrl} onClick={redirectByShortId} target='_blank'>http://localhost:8000/{shortUrl}</a>
     </>
   );
 };
